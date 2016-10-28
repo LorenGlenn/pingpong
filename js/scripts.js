4e1be6d51e4f40e945b409cb7daca7065b73a296
Loren Glenn <lorencglenn@gmail.com>
2016-10-28 08:25:29 -0700

    add functionality to display results on screen for user

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,32 @@
 var pingPong = function(userNumber) {
   var countToArray = [];
-  for (i = 1; i <= userNumber; i++) {
-    if (i % 15 == 0) {
-        countToArray.push("pingpong");
-    } else if (i % 5 == 0) {
-        countToArray.push("pong")
-    } else if (i % 3 == 0) {
-        countToArray.push("ping")
-    } else {
-        countToArray.push(i);
+  if (userNumber && userNumber > 0) {
+    for (i = 1; i <= userNumber; i++) {
+      if (i % 15 == 0) {
+          countToArray.push("pingpong");
+      } else if (i % 5 == 0) {
+          countToArray.push("pong")
+      } else if (i % 3 == 0) {
+          countToArray.push("ping")
+      } else {
+          countToArray.push(i);
+      }
     }
+  } else {
+    alert('please enter a positive number!')
   }
 
+  var answerString = countToArray.join(", ");
+  return answerString;
 };
 
 
 $(document).ready(function(){
   $('form#ping-pong').submit(function(event){
     var userNumber = parseInt($('#user-input').val());
-    pingPong(userNumber);
+    var result = pingPong(userNumber);
+    $('#answer').text(result);
+    $('#answer-section').show();
     event.preventDefault();
   });
 });
