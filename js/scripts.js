46e05a085d3066b5e4fb41629687cae48e50f7f0
Loren Glenn <lorencglenn@gmail.com>
2016-10-28 08:12:20 -0700

    add functionality to remove multiples of 5, 15, and 3 to be replaced by pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,13 @@ var pingPong = function(userNumber) {
   var countToArray = [];
   for (i = 1; i <= userNumber; i++) {
     if (i % 15 == 0) {
-          countToArray.push("ping");
+        countToArray.push("pingpong");
     } else if (i % 5 == 0) {
-      
+        countToArray.push("pong")
+    } else if (i % 3 == 0) {
+        countToArray.push("ping")
+    } else {
+        countToArray.push(i);
     }
   }
 
