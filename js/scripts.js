daa342bb84d41c48c24cd588a9d1223e9d3ab532
Loren Glenn <lorencglenn@gmail.com>
2016-10-28 08:09:06 -0700

    add functionality to count to the input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
+var pingPong = function(userNumber) {
+  var countToArray = [];
+  for (i = 1; i <= userNumber; i++) {
+    if (i % 15 == 0) {
+          countToArray.push("ping");
+    } else if (i % 5 == 0) {
+      
+    }
+  }
+
+};
 
 
 $(document).ready(function(){
-  
+  $('form#ping-pong').submit(function(event){
+    var userNumber = parseInt($('#user-input').val());
+    pingPong(userNumber);
+    event.preventDefault();
+  });
 });
